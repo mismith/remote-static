4d066659c2c0a50bf3cfed2e7326398cb12ad10a
Murray Smith <msmith@smarttech.com>
2016-11-30 16:45:46 -0700

    fix promise chain failure

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ const fetch = require('node-fetch');
 
 module.exports = function remoteStatic(remote) {
 	return function (req, res, next) {
-		fetch(`${remote}${req.url}`)
-			.then(fetched => {
-				if (!fetched.ok) return next();
+		fetch(`${remote}${req.url}`).then(fetched => {
+			if (!fetched.ok) return next();
 
-				res.type(fetched.headers.get('content-type')).status(fetched.status);
+			res.type(fetched.headers.get('content-type')).status(fetched.status);
 
-				return fetched.buffer();
-			})
-			.then(buffer => {
+			return fetched.buffer().then(buffer => {
 				res.send(buffer);
-			})
-			.catch(next);
+			});
+		})
+		.catch(next);
 	};
 };
